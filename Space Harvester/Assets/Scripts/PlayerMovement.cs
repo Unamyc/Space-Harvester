c8efe90d2e13c3f7ebafec7932dac26a2212481f
MathieuCaselles <mathieu.caselles@ynov.com>
2022-01-20 23:00:25 +0100

    fix couroutine

--- Space Harvester/Assets/Scripts/PlayerMovement.cs
+++ Space Harvester/Assets/Scripts/PlayerMovement.cs
@@ -124,7 +124,7 @@ public class PlayerMovement : MonoBehaviour
     
     private IEnumerator FixDetach()
     {
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(0.3f);
         canAttachToPlatform = true;
     }
 
